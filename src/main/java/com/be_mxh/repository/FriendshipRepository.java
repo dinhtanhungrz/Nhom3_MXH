4cd594fd108d440ca69f5c4e40a20b45db6e94c8
linhk123 <dokieulinh582005@gmail.com>
2026-02-12 20:26:22 +0700

    update

--- src/main/java/com/be_mxh/repository/FriendshipRepository.java
+++ src/main/java/com/be_mxh/repository/FriendshipRepository.java
@@ -12,19 +12,14 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
 
-    /**
-     * Tìm bạn chung giữa User A (myId) và User B (targetId).
-     * Logic: Lấy danh sách bạn của A GIAO với danh sách bạn của B.
-     */
     @Query(value = """
-        SELECT u.* FROM users u
+        SELECT u.* FROM mxh.users u
         WHERE u.id IN (
-            -- Lấy danh sách ID bạn bè của TÔI (myId)
             SELECT CASE 
                 WHEN f.requester_id = :myId THEN f.addressee_id 
                 ELSE f.requester_id 
             END
-            FROM friendships f
+            FROM mxh.friendships f
             WHERE (f.requester_id = :myId OR f.addressee_id = :myId)
             AND f.status = 'ACCEPTED'
         )
@@ -34,21 +29,21 @@ public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
                 WHEN f2.requester_id = :targetId THEN f2.addressee_id 
                 ELSE f2.requester_id 
             END
-            FROM friendships f2
+            FROM mxh.friendships f2
             WHERE (f2.requester_id = :targetId OR f2.addressee_id = :targetId)
             AND f2.status = 'ACCEPTED'
         )
         """,
             countQuery = """
-        SELECT COUNT(*) FROM users u
+        SELECT COUNT(*) FROM mxh.users u
         WHERE u.id IN (
             SELECT CASE WHEN f.requester_id = :myId THEN f.addressee_id ELSE f.requester_id END
-            FROM friendships f
+            FROM mxh.friendships f
             WHERE (f.requester_id = :myId OR f.addressee_id = :myId) AND f.status = 'ACCEPTED'
         )
         AND u.id IN (
             SELECT CASE WHEN f2.requester_id = :targetId THEN f2.addressee_id ELSE f2.requester_id END
-            FROM friendships f2
+            FROM mxh.friendships f2
             WHERE (f2.requester_id = :targetId OR f2.addressee_id = :targetId) AND f2.status = 'ACCEPTED'
         )
         """,
@@ -57,8 +52,8 @@ public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
                                  @Param("targetId") Long targetId,
                                  Pageable pageable);
     @Query(value = """
-    SELECT u.* FROM users u
-    JOIN friendships f ON (u.id = f.requester_id OR u.id = f.addressee_id)
+    SELECT u.* FROM mxh.users u
+    JOIN mxh.friendships f ON (u.id = f.requester_id OR u.id = f.addressee_id)
     WHERE (f.requester_id = :userId OR f.addressee_id = :userId)
     AND f.status = 'ACCEPTED'
     AND u.id != :userId
